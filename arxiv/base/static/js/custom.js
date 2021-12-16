9fd398671a352a59b5e850c9f84331b3cdd7e051
SBBCornell <sbb226@cornell.edu>
2021-12-16 15:31:43 -0500

    enable bootstrap5 offcanvas

--- arxiv/base/static/js/custom.js
+++ arxiv/base/static/js/custom.js
@@ -10,3 +10,9 @@ myAlert.addEventListener('closed.bs.alert', function () {
   // move focus to the content element which is below page alerts
   document.getElementById('content').focus()
 })
+
+/*Enable bootstrap off-canvas element*/
+var offcanvasElementList = [].slice.call(document.querySelectorAll('.offcanvas'))
+var offcanvasList = offcanvasElementList.map(function (offcanvasEl) {
+  return new bootstrap.Offcanvas(offcanvasEl)
+})
